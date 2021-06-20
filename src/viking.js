48f472f6de6cc142eb5c748b93057254e10e91e4
Alejandro Cazorla <acazorla72@gmail.com>
2021-06-20 13:53:26 +0200

    created viking class

--- src/viking.js
+++ src/viking.js
@@ -13,10 +13,32 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(name, health, strength)
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  } 
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super (health, strength)
+  }
+}
 
 // War
 class War {}
